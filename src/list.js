2b605c79a3d4ef51e44790eb2c05aebccdcfb283
Jeacson_Snake <Jeacson_Snake@outlook.com>
2022-01-17 23:01:42 +0800

    v1.0.0 实现了直接在行内进行修改的功能，是未导入vue之前的最终版

--- src/list.js
+++ src/list.js
@@ -1,13 +1,13 @@
 window.addEventListener("load", load());  //页面加载完毕调用load函数
-document.getElementById("clear-btn").onclick = clear();
 
-
-
-document.addEventListener('keyup', function (keyEvent) {
-  if(keyEvent.key == 'enter'){
-      addTodolist();
-  }
-});
+document.getElementById('add-list').addEventListener('click', function() {
+  document.addEventListener('keydown', function (keyEvent) {
+    if(keyEvent.key == "Enter"){
+      console.log('hi');
+        addTodolist();
+    }
+  });
+})
 
 
 function addTodolist() {
@@ -18,15 +18,14 @@ function addTodolist() {
 
   var tempDone = document.getElementById("add-list").value.trim(); //String切两边空格
   if (tempDone.length === 0){ //判断添加的内容是否为空
-      alert("添加的内容不能为空!");
       return;
   }
 
   obj_list.todo = tempDone;
 
-  todolist.push(obj_list);
+  todolist.push(obj_list); //将 obj_list 用 push 方式放入 todolist
 
-  saveData(todolist);
+  saveData(todolist); // 将 todolist 存储进本地
 
   document.getElementById("add-list").value = "";     //初始化输入框
   load();     //将用户输入的数据添加至dom节点
@@ -42,32 +41,28 @@ function load(){
   var todoString = "";
   var doneString = "";
 
+  // document.getElementById("add-list").focus();// 将光标指向输入框
 
-  document.getElementById("add-list").focus();
+  todolist = loadData();//调用loadData函数将本地存储数据调用进网页
 
-  todolist = loadData();
-
-  //todolist数组对象里若包含用户输入数据，则将其添加至dom节点；若为空对象，则初始化页面。
+  //若本地有相应存储数据，则将其添加至dom节点，反之初始化页面。
   if (todolist != null){
       for (var i=0; i<todolist.length; i ++){
           if(!todolist[i].done){
               todoString += "<li  class='dlist todolist'>"
-//通过onchange事件，复选框值有改变则调用update函数，并改变输入数据“done”属性的布尔值，这样
-//下次load()后，这段数据会进入不同的分组，未完成的事项分入已完成事项组，已完成事项分入未完成事项组
-//点击事项调用edit函数
-//点击“-”，调用remove函数
 
                   + "<input type='checkbox' " 
-                  + "onchange='update("+i+", \"done\", true)'> class='choose-box'"
-                  + "<span id='sp-"+i+"' onclick='edit("+i+")'>" + todolist[i].todo + "</span>" +
-                  "<a onclick='remove("+i+")'>-</a>" +
-                  "</li>";    //将每次用户输入的数据，通过节点<p>利用id标记，以便后续编辑功能定位
+                  + "onchange='update("+i+", \"done\", true)' class='choose-box'>" //通过onchange事件，当复选框值有改变，调用update函数，并改变输入数据“done”属性的布尔值
+                  + "<span id='sp-"+i+"' onclick='edit("+i+")'>" + todolist[i].todo + "</span>" //点击事项调用edit函数
+                  + "<input type='text' id='input-"+i+"' value=' ' style='display:none'>"
+                  + "<a onclick='remove("+i+")'>-</a>"//点击“-”，调用remove函数
+                  + "</li>";
           }
           else{
               doneString += "<li class='dlist donelist'>"
                   + "<input type='checkbox' "
                   + "onchange='update("+i+", \"done\", false)' class='choose-box' checked>"
-                  + "<span id='sp-"+i+"' onclick='edit("+i+")'>" + todolist[i].todo + "</span>"
+                  + "<span id='sp-"+i+"'>" + todolist[i].todo + "</span>"
                   + "<a onclick='remove("+i+")'>-</a>"
                   + "</li>";
           }
@@ -75,71 +70,86 @@ function load(){
       todo.innerHTML = todoString;
       done.innerHTML = doneString;
   }
+
   else {
       todo.innerHTML = "";
       done.innerHTML = "";
   }
+
 }
 
-function edit(i) {
-  var p = document.getElementById('sp-' + i),
-      pContent = p.innerHTML,
-      inputId;
+function edit(i) { // 这个函数用于直接修改list里的数据
+  var sp = document.getElementById('sp-' + i);
+  var inputId = document.getElementById('input-'+i);
 
-      p.innerHTML = "<input type='text' id='input-"+i+"' value='"+pContent+"'>";
-      inputId = document.getElementById('input-'+i);
-      inputId.focus();
-      inputId.setSelectionRange(0, inputId.value.length);
-      inputId.onblur = confirm;   //表单控件失去焦点，调用confirm函数，即对页面内容进行更新
-      inputId.onkeydown = enter;     //对按键事件进行监控
-//通过upadate函数对todolist数组相应项进行更新，将用户输入的内容写入到todolist数组相应项的todo属性中
+  var spContent = sp.innerHTML; // 保存原值
+
+  sp.style.display = 'none';
+  inputId.style.display = 'block';
+  inputId.value = spContent;
+  console.log(inputId);
+  inputId.setSelectionRange(0, inputId.value.length); // 选中文本框内的字符串
+  
+  inputId.focus();
+  inputId.addEventListener('blur', confirm);
+  inputId.addEventListener('keydown', function(keyEvent) {
+    console.log('lioi');
+    if (keyEvent.key == 'Enter'){
+        confirm();
+    }
+  });
+
   function confirm() {
-      if (inputId.value.length === 0) {
-          p.innerHTML = pContent;
-          alert("内容不能为空");
-      }
-      else {
-          update(i, "todo", inputId.value);   //修改事项内容后，更新数组里对应项"todo"属性的值，以便更新dom节点
-      }
-  }
-//结合key事件，按下enter键，调用confirm函数
-  function enter(e) {
-      if (e.key == 'enter'){
-          confirm();
-      }
+    if (inputId.value.length === 0) { // 如果用户更改后发现被清空
+        sp.innerHTML = spContent; //将原来未修改的数据回推给sp
+        alert("内容不能为空， 需要清除请点击右侧'-'号。");
+    }
+
+    else {
+        update(i, "todo", inputId.value); //通过upadate函数对todolist数组相应项进行更新，将用户输入的内容写入到todolist数组相应项的todo属性中
+    }
+
   }
 
 }
 
 function update(i, field, value) {
-  todolist[i][field] = value;
-  saveData(todolist);
-  load();
+  todolist[i][field] = value; // 将第 i 个元素中的 field 属性 改为 value 值
+  saveData(todolist); // 覆盖原本地缓存
+  load(); // 重新加载一次
 }
 
 function remove(i) {
-  todolist.splice(i, 1);
-
-  saveData(todolist); //相同名称的缓存会覆盖，更新缓存
-
-  load();
+  todolist.splice(i, 1); // 删除位于 i 的 1 个元素
+  saveData(todolist); // 覆盖原本地缓存
+  load(); // 重新加载一次
 }
 
 function saveData(data) {
-  localStorage.setItem("West2TodoList", JSON.stringify(data));   //JS对象转换成JSON对象存进本地缓存
+  console.log('opop');
+  localStorage.setItem("West2TodoList", JSON.stringify(data)); // JS数组转换成JSON对象存进本地
+  console.log('popod');
 }
 
 
 function loadData() {
+  console.log('uuuuu');
   var hisTory = localStorage.getItem("West2TodoList");
   if(hisTory !=null){
-      return JSON.parse(hisTory);     //JSON对象转换为JS对象
+    console.log('get');
+      return JSON.parse(hisTory); // JSON对象转换为JS数组
   }
-  else { return []; }
+  else {
+    console.log('no');
+    return []
+  }
 }
 
 function clear() {
-  localStorage.clear();
-  load();
+  console.log('ji');
+  localStorage.clear(); // 清除本地缓存
+  load(); // 重新加载一次
 }
+
+document.getElementById('clear-btn').addEventListener('click', clear); // 添加按钮清除
 
